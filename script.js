f4e0db52e9a81409ee54827df361e4e76175bbaa
velibor918 <velibor918@gmail.com>
2025-02-05 15:04:12 +0100

    Added focus functions to pw for message div  visibility

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ let lowercase = document.querySelector('#lowercase');
 let uppercase = document.querySelector('#uppercase');
 let pwDigit = document.querySelector('#digit');
 let pwLength = document.querySelector('#char-length');
+
+pw.addEventListener('focus', ()=> {
+    message.style.display ='block';
+});
+
+pw.addEventListener('focusout', ()=> {
+    message.style.display = 'none';
+})

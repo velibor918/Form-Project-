c96eb11cf80f8a07d17233f07a103b1b01f99a21
velibor918 <velibor918@gmail.com>
2025-02-05 15:18:26 +0100

    Added password validation function

--- script.js
+++ script.js
@@ -11,4 +11,41 @@ pw.addEventListener('focus', ()=> {
 
 pw.addEventListener('focusout', ()=> {
     message.style.display = 'none';
+});
+
+pw.addEventListener('keyup', ()=> {
+    let lowercaseValue = /[a-z]/g;
+    if(pw.value.match(lowercaseValue)){
+        lowercase.classList.remove("invalid");
+        lowercase.classList.add("valid");
+    } else {
+        lowercase.classList.remove('valid');
+        lowercase.classList.add('invalid');
+    }
+
+    let uppercaseValue = /[A-Z]/g;
+    if(pw.value.match(uppercaseValue)) {
+        uppercase.classList.remove('invalid');
+        uppercase.classList.add('valid');
+    } else {
+        uppercase.classList.remove('valid');
+        uppercase.classList.add('invalid');
+    }
+
+    let digitValue = /[0-9]/g;
+    if(pw.value.match(digitValue)) {
+        pwDigit.classList.remove('invalid');
+        pwDigit.classList.add('valid');
+    } else {
+        pwDigit.classList.remove('valid');
+        pwDigit.classList.add('invalid');
+    }
+
+    if(pw.value.length >= 8) {
+        pwLength.classList.remove('invalid');
+        pwLength.classList.add('valid');
+    } else {
+        pwLength.classList.remove('valid');
+        pwLength.classList.add('invalid');
+    }
 })

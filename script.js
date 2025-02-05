706a4f8a26373cacfa4e7257c097fd9ebcea7044
velibor918 <velibor918@gmail.com>
2025-02-05 14:55:46 +0100

    Added values for html elements

---
+++ script.js
@@ -0,0 +1,6 @@
+let pw = document.querySelector('#pw');
+let message = document.querySelector('#message');
+let lowercase = document.querySelector('#lowercase');
+let uppercase = document.querySelector('#uppercase');
+let pwDigit = document.querySelector('#digit');
+let pwLength = document.querySelector('#char-length');
